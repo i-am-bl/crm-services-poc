73609d9b36166a5c9b5135fe55c9ab940cdcf7b7
Brandon Lord <bklord8878@gmail.com>
2025-01-30 14:56:04 -0600

    documenting schems: account products

--- app/schemas/account_products.py
+++ app/schemas/account_products.py
@@ -1,72 +1,164 @@
 from datetime import date, datetime
 from typing import List, Optional
 
-from pydantic import UUID4, BaseModel
+from pydantic import UUID4, BaseModel, Field
 
 from ._variables import TimeStamp
 from .products import ProductsRes
 
 
 class AccountProducts(BaseModel):
-    account_uuid: UUID4
-    product_uuid: UUID4
-    start_on: Optional[date] = None
-    end_on: Optional[date] = None
+    """
+    Model representing an account product with associated product and contract dates.
+    """
+
+    account_uuid: UUID4 = Field(..., description="UUID of the associated account.")
+    product_uuid: UUID4 = Field(..., description="UUID of the associated product.")
+    start_on: Optional[date] = Field(
+        None, description="Start date of the account product contract.", default=None
+    )
+    end_on: Optional[date] = Field(
+        None, description="End date of the account product contract.", default=None
+    )
 
 
 class AccountProductsCreate(AccountProducts):
-    sys_created_at: datetime = TimeStamp
-    sys_created_by: Optional[UUID4] = None
+    """
+    Model representing an account product being created, including system metadata.
+    """
+
+    sys_created_at: datetime = Field(
+        TimeStamp, description="Timestamp of when the account product was created."
+    )
+    sys_created_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who created the account product.",
+        default=None,
+    )
 
 
 class AccountProductsUpdate(BaseModel):
-    start_on: Optional[date] = None
-    end_on: Optional[date] = None
-    sys_updated_at: datetime = TimeStamp
-    sys_updated_by: Optional[UUID4] = None
+    """
+    Model representing metadata for updating an existing account product.
+    """
+
+    start_on: Optional[date] = Field(
+        None, description="Start date of the account product contract.", default=None
+    )
+    end_on: Optional[date] = Field(
+        None, description="End date of the account product contract.", default=None
+    )
+    sys_updated_at: datetime = Field(
+        TimeStamp, description="Timestamp of when the account product was last updated."
+    )
+    sys_updated_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who last updated the account product.",
+        default=None,
+    )
 
 
 class AccountProductsDel(BaseModel):
-    sys_deleted_at: datetime = TimeStamp
-    sys_deleted_by: Optional[UUID4] = None
+    """
+    Model representing metadata for deleting an account product.
+    """
+
+    sys_deleted_at: datetime = Field(
+        TimeStamp, description="Timestamp of when the account product was deleted."
+    )
+    sys_deleted_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who deleted the account product.",
+        default=None,
+    )
 
 
 class AccountProductsRes(BaseModel):
-    id: int
-    uuid: UUID4
-    account_uuid: UUID4
-    product_uuid: UUID4
-    start_on: Optional[date] = None
-    end_on: Optional[date] = None
-    sys_created_at: datetime
-    sys_created_by: Optional[UUID4] = None
-    sys_updated_at: Optional[datetime] = None
-    sys_updated_by: Optional[UUID4] = None
+    """
+    Model representing the response data for an account product.
+    """
+
+    id: int = Field(..., description="Unique identifier of the account product entry.")
+    uuid: UUID4 = Field(..., description="UUID of the account product.")
+    account_uuid: UUID4 = Field(..., description="UUID of the associated account.")
+    product_uuid: UUID4 = Field(..., description="UUID of the associated product.")
+    start_on: Optional[date] = Field(
+        None, description="Start date of the account product contract.", default=None
+    )
+    end_on: Optional[date] = Field(
+        None, description="End date of the account product contract.", default=None
+    )
+    sys_created_at: datetime = Field(
+        ..., description="Timestamp of when the account product was created."
+    )
+    sys_created_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who created the account product.",
+        default=None,
+    )
+    sys_updated_at: Optional[datetime] = Field(
+        None,
+        description="Timestamp of when the account product was last updated.",
+        default=None,
+    )
+    sys_updated_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who last updated the account product.",
+        default=None,
+    )
 
     class Config:
         from_attributes = True
 
 
 class AccountProductsOrchPgRes(BaseModel):
-    total: int
-    page: int
-    limit: int
-    has_more: bool
-    data: Optional[List[ProductsRes]] = None
+    """
+    Represents a paginated response for account products.
+    """
+
+    total: int = Field(..., description="Total number of account products available.")
+    page: int = Field(..., description="Current page number.")
+    limit: int = Field(..., description="Number of account products per page.")
+    has_more: bool = Field(
+        ...,
+        description="Indicates if there are more account products available beyond the current page.",
+    )
+    data: Optional[List[ProductsRes]] = Field(
+        None,
+        description="List of product responses associated with the account products.",
+    )
 
 
 class AccountProductsPgRes(BaseModel):
-    total: int
-    page: int
-    limit: int
-    has_more: bool
-    # TODO: Standardize this to be data
-    account_products: Optional[List[AccountProductsRes]] = None
+    """
+    Represents a paginated response for account products.
+    """
+
+    total: int = Field(..., description="Total number of account products available.")
+    page: int = Field(..., description="Current page number.")
+    limit: int = Field(..., description="Number of account products per page.")
+    has_more: bool = Field(
+        ...,
+        description="Indicates if there are more account products available beyond the current page.",
+    )
+    account_products: Optional[List[AccountProductsRes]] = Field(
+        None, description="List of account product responses."
+    )
 
 
 class AccountProductsDelRes(AccountProductsRes):
-    sys_deleted_at: Optional[datetime] = None
-    sys_deleted_by: Optional[UUID4] = None
+    """
+    Represents the response data for a deleted account product, including deletion metadata.
+    """
+
+    sys_deleted_at: Optional[datetime] = Field(
+        None, description="Timestamp of when the account product was deleted."
+    )
+    sys_deleted_by: Optional[UUID4] = Field(
+        None,
+        description="UUID of the user who deleted the account product.",
+        default=None,
+    )
 
     class Config:
         from_attributes = True
