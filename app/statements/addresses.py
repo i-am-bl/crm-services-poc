577bcd7dfbd865c46ea2f6677c683f44357d9e3d
Brandon Lord <bklord8878@gmail.com>
2025-01-30 13:28:48 -0600

    documenting statements

--- app/statements/addresses.py
+++ app/statements/addresses.py
@@ -7,11 +7,29 @@ from ..utilities.data import set_empty_strs_null
 
 
 class AddressesStms:
+    """
+    A class responsible for constructing SQLAlchemy queries and statements for managing addresses.
+
+    ivars:
+    ivar: _model: Addresses: An instance of the Addresses model.
+    """
+
     def __init__(self, model: Addresses) -> None:
+        """
+        Initializes the AddressesStms class.
+
+        :param model: Addresses: An instance of the Addresses model.
+        :return None
+        """
         self._model: Addresses = model
 
     @property
     def model(self) -> Addresses:
+        """
+        Returns the model instance for addresses.
+
+        :return: Addresses: The Addresses model instance.
+        """
         return self._model
 
     def get_address(
@@ -20,6 +38,14 @@ class AddressesStms:
         parent_table: Literal["entities", "accounts"],
         address_uuid: UUID4,
     ) -> Select:
+        """
+        Selects an address by parent_uuid, parent_table, and address_uuid.
+
+        :param parent_uuid: UUID4: The UUID of the parent (entity or account).
+        :param parent_table: Literal["entities", "accounts"]: The table name (entities or accounts) associated with the address.
+        :param address_uuid: UUID4: The UUID of the address.
+        :return: Select: A Select statement for the address.
+        """
         addresses = self._model
         return Select(addresses).where(
             and_(
@@ -37,6 +63,15 @@ class AddressesStms:
         address_line2: str,
         city: str,
     ) -> Select:
+        """
+        Selects an address by its specific address fields.
+
+        :param parent_uuid: UUID4: The UUID of the parent (entity or account).
+        :param address_line1: str: The first line of the address.
+        :param address_line2: str: The second line of the address (if available).
+        :param city: str: The city of the address.
+        :return: Select: A Select statement for the address.
+        """
         addresses = self._model
         return Select(addresses).where(
             and_(
@@ -55,15 +90,23 @@ class AddressesStms:
         offset: int,
         limit: int,
     ) -> Select:
+        """
+        Selects addresses by parent_uuid and parent_table with pagination support.
+
+        :param parent_uuid: UUID4: The UUID of the parent (entity or account).
+        :param parent_table: Literal["entities", "accounts"]: The table name (entities or accounts) associated with the addresses.
+        :param offset: int: The number of records to skip.
+        :param limit: int: The number of records to return.
+        :return: Select: A Select statement for the addresses.
+        """
         addresses = self._model
         return (
-            (
-                Select(addresses).where(
-                    and_(
-                        addresses.parent_uuid == parent_uuid,
-                        addresses.parent_table == parent_table,
-                        addresses.sys_deleted_at == None,
-                    )
+            Select(addresses)
+            .where(
+                and_(
+                    addresses.parent_uuid == parent_uuid,
+                    addresses.parent_table == parent_table,
+                    addresses.sys_deleted_at == None,
                 )
             )
             .offset(offset=offset)
@@ -75,9 +118,16 @@ class AddressesStms:
         parent_uuid: UUID4,
         parent_table: Literal["entities", "accounts"],
     ) -> Select:
+        """
+        Selects the count of addresses by parent_uuid and parent_table.
+
+        :param parent_uuid: UUID4: The UUID of the parent (entity or account).
+        :param parent_table: Literal["entities", "accounts"]: The table name (entities or accounts) associated with the addresses.
+        :return: Select: A Select statement with the count of addresses.
+        """
         addresses = self._model
         return (
-            Select(func.count())
+            select(func.count())
             .select_from(addresses)
             .where(
                 and_(
@@ -91,10 +141,19 @@ class AddressesStms:
     def update_address(
         self,
         parent_uuid: UUID4,
-        addresses_uuid: UUID4,
+        address_uuid: UUID4,
         parent_table: Literal["entities", "accounts"],
         address_data: object,
     ) -> Update:
+        """
+        Updates an address by parent_uuid, address_uuid, and parent_table.
+
+        :param parent_uuid: UUID4: The UUID of the parent (entity or account).
+        :param address_uuid: UUID4: The UUID of the address.
+        :param parent_table: Literal["entities", "accounts"]: The table name (entities or accounts) associated with the address.
+        :param address_data: object: The data to update the address with.
+        :return: Update: An Update statement for the address.
+        """
         addresses = self._model
         return (
             update(addresses)
@@ -102,7 +161,7 @@ class AddressesStms:
                 and_(
                     addresses.parent_uuid == parent_uuid,
                     addresses.parent_table == parent_table,
-                    addresses.uuid == addresses_uuid,
+                    addresses.uuid == address_uuid,
                     addresses.sys_deleted_at == None,
                 )
             )
