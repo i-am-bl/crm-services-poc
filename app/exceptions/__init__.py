7fba9d4bce04ccea3edaa27741266194409165b9
Brandon Lord <bklord8878@gmail.com>
2025-01-30 14:22:25 -0600

    documenting exceptions package

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,3 +1,10 @@
+"""
+The exceptions package contains multiple modules, each defining custom exception classes. 
+These exception classes inherit from the base class CRMExceptions, which in turn inherits from 
+Python's built-in Exception class. Each custom exception class is associated with a default message 
+and provides logging capabilities, ensuring better error handling and traceability across the application.
+"""
+
 from .account_contracts import *
 from .account_lists import *
 from .account_products import *
--- app/exceptions/crm_exceptions.py
+++ app/exceptions/crm_exceptions.py
@@ -1,15 +1,26 @@
 from typing import Optional
-
 from ..utilities.logger import logger
 
 
 class CRMExceptions(Exception):
-    """Base class for all exceptions"""
+    """
+    Base class for all custom exceptions in the CRM application.
+
+    This exception class inherits from Python's built-in Exception class and serves as the base
+    for creating custom exceptions in the CRM system. It provides the functionality to log
+    the exception message whenever an exception is raised.
+
+    :param message: The error message to display when the exception is raised.
+                    Defaults to None, meaning no message is provided unless explicitly specified.
+    :param args: Additional positional arguments to pass to the parent exception class.
+    :param kwargs: Additional keyword arguments to pass to the parent exception class.
+    """
 
     def __init__(self, message: Optional[str] = None, *args: object, **kwargs) -> None:
         super().__init__(message, *args, **kwargs)
         if message:
             self.log_exception(message=message)
 
-    def log_exception(self, message):
+    def log_exception(self, message: str):
+        """Logs the exception message at the warning level with the class name."""
         logger.warning(f"{self.__class__.__name__}: {message}")
