a2e748449e7dbc4192389195e6367d86d7a26b74
Brandon Lord <bklord8878@gmail.com>
2025-01-30 14:35:27 -0600

    documenting database package

--- app/database/database.py
+++ app/database/database.py
@@ -1,33 +1,55 @@
 from contextlib import asynccontextmanager
 from typing import List
-
 from config import settings as set
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
-
+# Connection string for database connection.
 connection_string = f"{set.db_connector}://{set.db_usrnm}:{set.db_pwd}@{set.db_hst}:{set.db_port}/{set.db_nm}"
 
 try:
+    # Create asynchronous engine for the database connection.
     async_engine = create_async_engine(connection_string, echo=True)
-    print("connection made asyncronously")
+    print("connection made asynchronously")
 except:
     print("ConnectionError")
 
+# Factory for creating asynchronous sessions with SQLAlchemy.
 LocalAsyncSession = async_sessionmaker(
     bind=async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
+# Base class for SQLAlchemy models.
 Base = declarative_base()
 
 
 async def init_db_tables(model: object):
+    """
+    Initializes database tables based on the provided model.
+
+    This function creates all the tables defined in the SQLAlchemy model's `Base.metadata`.
+
+    :param model: The SQLAlchemy model class that defines the table structure.
+    :type model: object
+    :return: None
+    :raises: SQLAlchemy exceptions if there is an issue with creating tables.
+    """
     async with async_engine.begin() as conn:
         await conn.run_sync(model.Base.metadata.create_all)
 
 
 async def init_db_table_schema(schema: str):
+    """
+    Initializes a database schema if it does not already exist.
+
+    This function checks if a schema exists, and if not, creates the schema in the database.
+
+    :param schema: The name of the schema to create.
+    :type schema: str
+    :return: None
+    :raises: SQLAlchemy exceptions if there is an issue with schema creation.
+    """
     async with async_engine.begin() as conn:
         stm = f"create schema if not exists {schema};"
         await conn.execute(text(stm))
@@ -35,11 +57,31 @@ async def init_db_table_schema(schema: str):
 
 
 async def init_db_table_schema_factory(schemas: List[str]):
+    """
+    Initializes multiple database schemas.
+
+    This function iterates over a list of schema names and creates each schema if it does not exist.
+
+    :param schemas: A list of schema names to create.
+    :type schemas: List[str]
+    :return: None
+    :raises: SQLAlchemy exceptions if there is an issue with schema creation.
+    """
     for schema in schemas:
         await init_db_table_schema(schema=schema)
 
 
 async def get_db():
+    """
+    Provides a session for database operations.
+
+    This is an asynchronous generator that yields a database session object for use in operations,
+    ensuring proper session closure when done.
+
+    :return: The database session.
+    :rtype: AsyncSession
+    :raises: SQLAlchemy exceptions if there is an issue with session creation.
+    """
     async with LocalAsyncSession() as db:
         try:
             yield db
@@ -49,5 +91,17 @@ async def get_db():
 
 @asynccontextmanager
 async def transaction_manager(db: AsyncSession):
+    """
+    Manages database transactions for a given session.
+
+    This context manager starts a transaction, yields the session for use in operations,
+    and commits or rolls back the transaction depending on the outcome.
+
+    :param db: The database session to manage transactions.
+    :type db: AsyncSession
+    :yield: The database session to use for performing transactions.
+    :rtype: AsyncSession
+    :raises: SQLAlchemy exceptions if there is an issue with transaction management.
+    """
     async with db.begin():
         yield db
--- app/database/operations.py
+++ app/database/operations.py
@@ -1,69 +1,126 @@
+from typing import Any, List
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import Select, Update
 from ..utilities.logger import logger
 from ..utilities.data import m_dumps
 
 
 class Operations:
     """
-    Class contains utilities that are specific to database interaction.
+    Utility class for interacting with the database.
+
+    This class provides static methods for common database operations such as
+    fetching rows, counting rows, and inserting instances. It works asynchronously
+    with SQLAlchemy's `AsyncSession`.
+
+    ivars:
+        ivar: _logger: A logger utility for logging database operations.
+        varType: logger
+        ivar: _m_dumps: A utility function for serializing data before inserting it into the model.
+        varType: m_dumps
     """
 
     @staticmethod
     async def return_one_row(
         service: str,
-        statement: object,
+        statement: Select | Update,
         db: AsyncSession,
-    ):
+    ) -> Any:
+        """
+        Executes a SQL statement and returns a single row.
+
+        This method is used for queries expected to return a single row,
+        fetching the result using `scalars().first()`.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param statement: The SQL statement to execute. It can be a `Select` or `Update` statement.
+        :type statement: Select | Update
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: The first row of the result or `None` if no rows are returned.
+        :rtype: Any
+        """
         logger.info({"statement": str(statement)})
-        logger.info(f"Executing database operation for sevice: {service}.")
+        logger.info(f"Executing database operation for service: {service}.")
         result = await db.execute(statement=statement)
         return result.scalars().first()
 
     @staticmethod
     async def return_all_rows(
         service: str,
-        statement: object,
+        statement: Select | Update,
         db: AsyncSession,
-    ):
+    ) -> List[Any]:
         """
-        Returns all rows when select * is used.
+        Executes a SQL statement and returns all rows when `SELECT *` is used.
+
+        This method is used when the query is expected to return all columns
+        from a table using `SELECT *`.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param statement: The SQL statement to execute. It can be a `Select` or `Update` statement.
+        :type statement: Select | Update
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: A list of all rows returned by the query.
+        :rtype: List[Any]
         """
         logger.info({"statement": str(statement)})
-        logger.info(f"Executing database operation for sevice: {service}.")
+        logger.info(f"Executing database operation for service: {service}.")
         result = await db.execute(statement=statement)
         return result.scalars().all()
 
     @staticmethod
     async def return_all_rows_and_values(
         service: str,
-        statement: object,
+        statement: Select | Update,
         db: AsyncSession,
-    ):
+    ) -> List[tuple]:
         """
-        Returns all rows and values when select * is NOT used.
+        Executes a SQL statement and returns all rows and values when `SELECT *` is not used.
 
-        example: select column1, column2, ... from table
+        This method is used when specific columns are selected in the query,
+        for example: `SELECT column1, column2 FROM table`.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param statement: The SQL statement to execute. It can be a `Select` or `Update` statement.
+        :type statement: Select | Update
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: A list of rows with their corresponding values.
+        :rtype: List[tuple]
         """
         logger.info({"statement": str(statement)})
-        logger.info(f"Executing database operation for sevice: {service}.")
+        logger.info(f"Executing database operation for service: {service}.")
         result = await db.execute(statement=statement)
         return result.all()
 
     @staticmethod
     async def return_count(
         service: str,
-        statement: object,
+        statement: Select | Update,
         db: AsyncSession,
-    ):
+    ) -> int:
         """
-        Returns one value.
+        Executes a SQL statement and returns a count value (typically used for pagination).
 
-        Specifically used for selecting count for pagination purposes.
-        example: select count(1) from table
+        This method is specifically used to return a single value, like the
+        result of `SELECT COUNT(*) FROM table`.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param statement: The SQL statement to execute. It can be a `Select` or `Update` statement.
+        :type statement: Select | Update
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: The count value returned by the query.
+        :rtype: int
         """
         logger.info({"statement": str(statement)})
-        logger.info(f"Executing database operation for sevice: {service}.")
+        logger.info(f"Executing database operation for service: {service}.")
         result = await db.execute(statement=statement)
         return result.scalar()
 
@@ -73,13 +130,27 @@ class Operations:
         model: object,
         data: object,
         db: AsyncSession,
-    ):
+    ) -> object:
         """
-        Commits one item or inserts one row to the database.
+        Inserts a single instance (row) into the database.
+
+        This method commits one item into the database after dumping the data
+        into the model.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param model: The model class to insert the data into.
+        :type model: object
+        :param data: The data to insert into the model.
+        :type data: object
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: The inserted instance.
+        :rtype: object
         """
         logger.info("Dumping data into model.")
         instance = model(**m_dumps(data=data))
-        logger.info(f"Executing database operation for sevice: {service}.")
+        logger.info(f"Executing database operation for service: {service}.")
         db.add(instance=instance)
         logger.info(f"Committing entry to the database for service: {service}.")
         return instance
@@ -90,12 +161,26 @@ class Operations:
         model: object,
         data: object,
         db: AsyncSession,
-    ):
+    ) -> List[object]:
         """
-        Commits many items or rows to the database.
+        Inserts multiple instances (rows) into the database.
+
+        This method commits many items into the database after dumping the data
+        into the model.
+
+        :param service: The name of the service requesting the operation.
+        :type service: str
+        :param model: The model class to insert the data into.
+        :type model: object
+        :param data: A list of data to insert into the model.
+        :type data: object
+        :param db: The database session.
+        :type db: AsyncSession
+        :return: A list of inserted instances.
+        :rtype: List[object]
         """
         logger.info("Dumping data into model.")
         instances = [model(**m_dumps(instance)) for instance in data]
         db.add_all(instances=instances)
-        logger.info(f"Committing entry to the database for service: {service}.")
+        logger.info(f"Committing entries to the database for service: {service}.")
         return instances
