32b1c56bf68d27c60ee7ce2863e80296745f8b13
Brandon Lord <bklord8878@gmail.com>
2025-01-30 13:51:01 -0600

    documenting statements

---
+++ app/statements/__init__.py
@@ -0,0 +1,5 @@
+"""
+The `statements` package contains several modules, each of which defines a class responsible for
+constructing SQLAlchemy statements. These classes generate SQL statements that return either 
+`Select` or `Update` objects.
+"""
